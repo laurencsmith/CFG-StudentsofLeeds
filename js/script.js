b258d285a0b462cb41951cceea474f9ce0fe0b32
Lauren Smith <laurenclairesmith@live.co.uk>
2019-03-18 20:09:05 +0000

    back to top button

--- js/script.js
+++ js/script.js
@@ -41,3 +41,18 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
