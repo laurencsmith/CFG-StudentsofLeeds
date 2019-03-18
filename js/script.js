52e3cdb608b350b499366239c1cae83c81034d13
kimoshinowo <47217534+kimoshinowo@users.noreply.github.com>
2019-03-18 19:49:39 +0000

    boxes expand when read more is clicked

--- js/script.js
+++ js/script.js
@@ -35,9 +35,11 @@ function myFunction() {
     dots.style.display = "inline";
     btnText.innerHTML = "Read more";
     moreText.style.display = "none";
+    document.getElementById("middle").style.height = "570px";
   } else {
     dots.style.display = "none";
     btnText.innerHTML = "Read less";
     moreText.style.display = "inline";
+    document.getElementById("middle").style.height = "770px";
   }
 }
